235528bf4e3c4f041f4c892945aa28bc9cc76d7b
Benedikt Schwering <mail@benedikt-schwering.de>
2021-11-10 20:35:46 +0100

    version 1

--- client/main.py
+++ client/main.py
@@ -1,28 +1,69 @@
 import obd
 from time import sleep
+import sys
+import time
+import threading, http.server, socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-connection = obd.OBD() # auto-connects to USB or RF port
+values = {}
 
-def getValue(command, unit=None):
-    try:
-        response = connection.query(obd.commands.SPEED).value
+class OBD:
+    connection = None
 
-        if unit:
-            response = response.to(unit)
+    def __init__(self):
+        while True:
+            self.connection = obd.OBD()
+            self.values = {}
 
-        return float(response.magnitude)
-    except:
-        return float(0)
+            if self.connection.is_connected():
+                print("Connected to OBDII adapter")
+                break
+
+            sleep(5)
+
+        while True:
+            self.getValue(obd.commands.SPEED, "speed", "kph")
+            self.getValue(obd.commands.RPM, "rpm")
+            self.getValue(obd.commands.THROTTLE_POS, "throttlePos")
+            self.getValue(obd.commands.OIL_TEMP, "oilTemp")
+            self.getValue(obd.commands.ENGINE_LOAD, "engineLoad")
+            self.getValue(obd.commands.COOLANT_TEMP, "coolantTemp")
+
+            sleep(1)
+
+    def getValue(self, command, field, unit=None):
+        try:
+            response = self.connection.query(command).value
+
+            if unit:
+                response = response.to(unit)
+
+            values[field] = float(response.magnitude)
+        except:
+            values[field] = float(0)
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+        if "/json" in self.path:
+            self.wfile.write(bytes(str(dict(values)).replace("'", '"') , encoding="utf8"))
+
+class Server:
+    def __init__(self):
+        httpd = HTTPServer(('', 2148), SimpleHTTPRequestHandler)
+        httpd.serve_forever()
+
+
+obdThread = threading.Thread(target=OBD)
+obdThread.daemon = True
+obdThread.start()
+
+serverThread = threading.Thread(target=Server)
+serverThread.daemon = True
+serverThread.start()
 
 while True:
-    print('')
-    print('')
-    
-    print('SPEED: ' + getValue(obd.commands.SPEED, "kph"))
-    print('RPM: ' + getValue(obd.commands.RPM))
-    print('THROTTLE_POS: ' + getValue(obd.commands.THROTTLE_POS))
-    print('OIL_TEMP: ' + getValue(obd.commands.OIL_TEMP))
-    print('ENGINE_LOAD: ' + getValue(obd.commands.ENGINE_LOAD))
-    print('COOLANT_TEMP: ' + getValue(obd.commands.COOLANT_TEMP))
-
-    sleep(1)
+    sleep(100)

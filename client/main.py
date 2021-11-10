767f5874bc0d73ba0108948fe40366ecdedb2dfa
Benedikt Schwering <mail@benedikt-schwering.de>
2021-11-10 17:22:04 +0100

    test

--- client/main.py
+++ client/main.py
@@ -7,11 +7,11 @@ while True:
     print('')
     print('')
     
-    print('SPEED: ' + connection.query(obd.commands.SPEED).value.to("kmh"))
-    print('RPM: ' + connection.query(obd.commands.RPM).value)
-    print('THROTTLE_POS: ' + connection.query(obd.commands.THROTTLE_POS).value)
-    print('OIL_TEMP: ' + connection.query(obd.commands.OIL_TEMP).value)
-    print('ENGINE_LOAD: ' + connection.query(obd.commands.ENGINE_LOAD).value)
-    print('COOLANT_TEMP: ' + connection.query(obd.commands.COOLANT_TEMP).value)
+    print('SPEED: ' + str(float(connection.query(obd.commands.SPEED).value.to("kph").magnitude)))
+    print('RPM: ' + str(float(connection.query(obd.commands.RPM).value)))
+    print('THROTTLE_POS: ' + str(float(connection.query(obd.commands.THROTTLE_POS).value)))
+    print('OIL_TEMP: ' + str(float(connection.query(obd.commands.OIL_TEMP).value)))
+    print('ENGINE_LOAD: ' + str(float(connection.query(obd.commands.ENGINE_LOAD).value)))
+    print('COOLANT_TEMP: ' + str(float(connection.query(obd.commands.COOLANT_TEMP).value)))
 
     sleep(1)

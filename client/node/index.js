341718974c20a37174f3b17f48a0c6e524f7f1c2
Benedikt Schwering <mail@benedikt-schwering.de>
2021-11-10 17:03:22 +0100

    node test

---
+++ client/node/index.js
@@ -0,0 +1,23 @@
+var OBDReader = require('obd2-over-serial');
+var options = {};
+options.baudRate = 115200;
+var serialOBDReader = new OBDReader("/dev/ttyusb0", options);
+var dataReceivedMarker = {};
+
+serialOBDReader.on('dataReceived', function (data) {
+    console.log(data);
+    dataReceivedMarker = data;
+});
+
+serialOBDReader.on('connected', function (data) {
+    this.addPoller("vss");
+    this.addPoller("rpm");
+    this.addPoller("temp");
+    this.addPoller("load_pct");
+    this.addPoller("map");
+    this.addPoller("frp");
+
+    this.startPolling(2000); //Polls all added pollers each 2000 ms.
+});
+
+serialOBDReader.connect();

76b58446b392d6d10f06ea00a3c1a44f06cc74b8
Benedikt Schwering <mail@benedikt-schwering.de>
2022-06-24 21:35:08 +0200

    dev

--- camera/src/main.py
+++ camera/src/main.py
@@ -1,7 +1,3 @@
 import os, time
 
-# os.system("fswebcam -r 1920x1080 --no-banner /usr/src/app/out/test.png")
-
-
-while True:
-    time.sleep(1000)
+os.system("fswebcam -r 1920x1080 --no-banner out/test.png")

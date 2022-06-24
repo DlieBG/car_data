27a4bd0bcaa432755fe7a2b158419cd7664611b1
Benedikt Schwering <mail@benedikt-schwering.de>
2022-06-24 21:24:20 +0200

    fswebcam

--- camera/src/main.py
+++ camera/src/main.py
@@ -1,12 +1,3 @@
-from cv2 import VideoCapture, imwrite, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT
+import os
 
-cam = VideoCapture(0)
-cam.set(CAP_PROP_FRAME_WIDTH, 1920)
-cam.set(CAP_PROP_FRAME_HEIGHT, 1080)
-
-result, image = cam.read()
-  
-if result:
-    imwrite("out/test.png", image)
-else:
-    print("No image detected. Please! try again")
+os.system("fswebcam -r 1920x1080 --no-banner out/test.png")
